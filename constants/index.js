ba8f85e889f32474187f63c38c49b76327e7cd9d
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-09-09 16:44:57 +0200

    Add 'get started' section

--- constants/index.js
+++ constants/index.js
@@ -75,3 +75,9 @@ export const socials = [
         url: '/facebook-svgrepo-com.svg'
     },
 ];
+
+export const startingFeatures = [
+    'Find a world that suits you and you want to enter',
+    'Enter the world by reading basmalah to be safe',
+    'No need to beat around the bush, just stay on the gas and have fun'
+];
